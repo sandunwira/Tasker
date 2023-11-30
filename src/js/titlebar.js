ac0ffe8df06575160a0c860bc22daf8cf4c05fe4
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-11-30 10:48:04 +0530

    Initialized Basic App Template

---
+++ src/js/titlebar.js
@@ -0,0 +1,25 @@
+function titlebarHTML() {
+	var html = '<div class="titlebar-container">';
+
+	html += '<div class="title">';
+	html += '<img src="../assets/images/logos/logo-text.svg">';
+	html += '</div>';
+
+	html += '<div class="controls">';
+
+	html += '<div class="icon" id="titlebar-minimize" title="Minimize">';
+	html += '<img src="../assets/images/icons/minimize.svg">';
+	html += '</div>';
+
+	html += '<div class="icon" id="titlebar-close" title="Close">';
+	html += '<img src="../assets/images/icons/close.svg">';
+	html += '</div>';
+
+	html += '</div>';
+
+	html += '</div>';
+	return html;
+}
+
+var container = document.getElementById('titlebar');
+container.innerHTML = titlebarHTML();
--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// const { appWindow } = window.__TAURI__.window;
+// == TITLEBAR START ================================================================================ //
+const { appWindow } = window.__TAURI__.window;
+document.getElementById('titlebar-minimize').addEventListener('click', () => appWindow.minimize())
+document.getElementById('titlebar-close').addEventListener('click', () => appWindow.close())
+// ================================================================================== TITLEBAR END == //
+
+// == DISABLE CONTEXT MENU START ==================================================================== //
+document.addEventListener('contextmenu', function (event) {
+	event.preventDefault();
+});
+// ====================================================================== DISABLE CONTEXT MENU END == //
